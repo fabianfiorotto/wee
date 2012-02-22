a95c99110aabfc96738d7f9afdfb4a2b005291cf
Fabian Fiorotto <fabian@Inspiron-N4020.(none)>
2012-02-22 16:08:00 -0300

    fix problem with component initialization in rails

--- lib/wee/rails_integration.rb
+++ lib/wee/rails_integration.rb
@@ -17,13 +17,13 @@ if defined? ActionController::Base then
 
     def self.publish_wee_applications(*arg)
      actions =  arg[0]
-     actions.each_key do |action|
-      if actions[action].respond_to? "instanciate" then
-        root_component =  actions[action].instanciate
-      else
-        root_component =  actions[action].new
-      end   
+     actions.each_key do |action| 
       wee_application = Wee::Application.new {
+          if actions[action].respond_to? "instanciate" then
+            root_component =  actions[action].instanciate
+          else
+            root_component =  actions[action].new
+          end  
           Wee::Session.new(root_component,Wee::Session::ThreadSerializer.new)
       }
       controller = self.to_s.sub(/Controller$/, '').underscore
